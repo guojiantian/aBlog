0e6d031f327a18e8a857e0766cb516400ec3ca48
Leon.G <mailguoliang@163.com>
2016-01-15 17:20:39 +0800

    add file app.py and orm.py

---
+++ www/orm.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+__author__ = 'guojiantian'
+
+
+import logging
+import asyncio, aiomysql
+
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connections pool...')
+    global _pool
+    _pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+    )
+
+
+# coroute to execute SELECT from database
+@asyncio.coroutine
+def select(sql, args, size=None):
+    log(sql, args)
+    global _pool
+    with (yield from _pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s', len(rs))
+        return rs
+
+
+# coroute to execute INSERT, UPDATE, DELETE
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql, args)
+    global _pool
+    with (yield from _pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self,key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'." % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s:%s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+
+if __name__ == '__main__':
+    mo = Model({'bob':80, 'alice':90})
+    print(mo['bob'])
+    print(mo.bob)
+
+
+
